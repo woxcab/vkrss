7ed9e27ce8deb7da56f0bf654def579913906dc5
woxcab <woxcab@users.noreply.github.com>
2017-07-29 09:27:25 +0300

    Interpret notices and warnings as errors

--- Vk2rss.php
+++ Vk2rss.php
@@ -19,6 +19,17 @@
 
 require_once('utils.php');
 
+set_error_handler('exceptions_error_handler');
+
+function exceptions_error_handler($severity, $message, $filename, $lineno) {
+    if (error_reporting() == 0) {
+        return;
+    }
+    if (error_reporting() & $severity) {
+        throw new ErrorException($message, 0, $severity, $filename, $lineno);
+    }
+}
+
 class Vk2rss
 {
     const HASH_TAG_PATTERN = '#([а-яёА-ЯЁa-zA-Z0-9_]+)(?:@[a-zA-Z0-9_]+)?';
