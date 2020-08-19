9bd7bfde433b92579cfac7a500f85e5f3fe58bb4
woxcab <woxcab@users.noreply.github.com>
2020-08-19 15:33:45 +0300

    Fix warning about case-insensetive constants. Fix #10

--- FeedWriter.php
+++ FeedWriter.php
@@ -3,9 +3,9 @@
 // RSS 0.91, 0.92, 0.93 and 0.94  Officially obsoleted by 2.0
 // So, define constants for RSS 1.0, RSS 2.0 and ATOM 	
 
-define('RSS1', 'RSS 1.0', true);
-define('RSS2', 'RSS 2.0', true);
-define('ATOM', 'ATOM', true);
+define('RSS1', 'RSS 1.0');
+define('RSS2', 'RSS 2.0');
+define('ATOM', 'ATOM');
 
 /**
  * Univarsel Feed Writer class
